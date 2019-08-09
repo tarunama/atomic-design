99024bd1f14ef1c28317819d53d9ef03de7e0b79
tarunama <t.yamamoto@smarttrade.co.jp>
2019-08-09 14:03:29 +0900

    Get weather data of current location

--- src/Pages/Top.vue
+++ src/Pages/Top.vue
@@ -14,6 +14,26 @@ export default {
   components: {
     Content,
     Header
+  },
+  mounted () {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(async position => {
+        const lat = position.coords.latitude
+        const lon = position.coords.longitude
+        const apiURL = process.env.VUE_APP_DARK_SKY_API_URL
+
+        try {
+          const res = await this.$_axios.get(`${apiURL}${lat},${lon}`)
+          console.log(res)
+        } catch (err) {
+          // TODO: わかりやすいlogを出力するようにする
+          console.error(err)
+        }
+      })
+    } else {
+      // HACKME: richなdialogを出す
+      alert('このブラウザでは位置情報を取得できません')
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,15 @@ const path = require('path')
 
 module.exports = {
   configureWebpack: {
-      resolve: {
-        alias: {
-          '@': path.join(__dirname, 'src/')
-        }
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src/')
       }
+    }
+  },
+  devServer: {
+    public: 'localhost',
+    host: '0.0.0.0',
+    port: '8082'
   }
 }
