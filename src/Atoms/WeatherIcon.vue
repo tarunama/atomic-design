0a2ba236a8334c0096cf5e6bdb718f8ef54209d8
tarunama <t.yamamoto@smarttrade.co.jp>
2019-08-13 14:40:35 +0900

    Add WeatherIcon Atom

---
+++ src/Atoms/WeatherIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <img :src="iconPath" />
+</template>
+
+<script>
+export default {
+  name: 'WeatherIcon',
+  props: {
+    wData: {
+      type: Object,
+      required: false
+    }
+  },
+  computed: {
+    iconPath () {
+      if (!this.wData) return
+
+      const iconName = this.wData.data.daily.icon
+      const iconPath = require(`@/assets/img/icon/${iconName}.png`)
+      return iconPath
+    }
+  }
+}
+</script>
