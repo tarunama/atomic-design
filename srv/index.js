af4c8ec74fd2113fb498b77bb8d999087446517c
tarunama <sakeharami@gmail.com>
2019-12-02 23:16:52 +0900

    Allow eslint

--- srv/index.js
+++ srv/index.js
@@ -1,34 +1,23 @@
-import express from 'express';
-// import socketIO from "socket.io";
+import express from 'express'
 import axios from 'axios'
 
 const cors = require('cors')
 
-export default (app, http) => {
-  app.use(express.json());
-  
+export default (app, _) => {
+  app.use(express.json())
+
   app.use(cors({
     credentials: true
   }))
-  
+
   app.post('/weather', (req, res) => {
     res.setHeader('Access-Controll-Allow-Origin', req.headers.origin)
     axios.get(`${process.env.VUE_APP_DARK_SKY_API_URL}${req.body.lat},${req.body.lon}?units=si&lang=ja`)
-      .then(result=> {
-        res.json({data: result.data})
+      .then(result => {
+        res.json({ data: result.data })
       })
       .catch(error => {
-        res.status(500).send({msg: `Error: ${error}`})
+        res.status(500).send({ msg: `Error: ${error}` })
       })
   })
-  
-  // optional support for socket.io
-  // 
-  // let io = socketIO(http);
-  // io.on("connection", client => {
-  //   client.on("message", function(data) {
-  //     // do something
-  //   });
-  //   client.emit("message", "Welcome");
-  // });
 }
